cfcde6d3761594337cddef5c5a45acc5781acf2c
Diego Torres Fuerte <dtorresf@redhat.com>
2023-01-23 11:18:03 -0700

    companies form: basic info

--- src/app/modules/catalogs/catalogs.module.ts
+++ src/app/modules/catalogs/catalogs.module.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { RouterModule, Routes } from '@angular/router';
 import { CompaniesComponent } from './companies/companies.component';
-import { HomeComponent } from './home/home.component';
 import { NewCompanyComponent } from './companies/new-company/new-company.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
@@ -45,7 +49,9 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     MatButtonModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class CatalogsModule { }
--- src/app/modules/catalogs/companies/new-company/new-company.component.ts
+++ src/app/modules/catalogs/companies/new-company/new-company.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-company.component.sass']
 })
 export class NewCompanyComponent {
-
+  
 }
