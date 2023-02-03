ffffeab25b72af960ea1d1831873dfe62b95fc2d
Diego Torres Fuerte <dtorresf@redhat.com>
2023-02-03 12:24:47 -0700

    show user name and picture

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
@@ -7,6 +7,13 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.sass']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  profileJson: any = null;
+
   constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) { }
+  ngOnInit(): void {
+    this.auth.user$.subscribe(
+      (profile) => (this.profileJson = JSON.stringify(profile, null, 2)),
+    );
+  }
 }
