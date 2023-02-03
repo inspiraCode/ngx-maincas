929c82d7187e189c41bc9e5725ffbe9e8fdc4bc3
Diego Torres Fuerte <dtorresf@redhat.com>
2023-02-03 12:09:14 -0700

    protect dashboard routes with AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
@@ -9,7 +10,8 @@ const routes: Routes = [
   },
   {
     path: "dashboard",
-    loadChildren: () => import("./modules/dashboard/dashboard.module").then(m => m.DashboardModule)
+    loadChildren: () => import("./modules/dashboard/dashboard.module").then(m => m.DashboardModule),
+    canActivate: [AuthGuard]
   },
   // Other routes
   { path: '404', component: PagenotfoundComponent },
