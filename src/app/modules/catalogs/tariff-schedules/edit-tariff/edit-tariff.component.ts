c9f9fecd3abc745b101f1d2946b8c5b99e920fe4
Diego Torres Fuerte <dtorresf@redhat.com>
2023-02-05 09:58:02 -0700

    ensure onInit for tariff edit form

--- src/app/modules/catalogs/tariff-schedules/edit-tariff/edit-tariff.component.ts
+++ src/app/modules/catalogs/tariff-schedules/edit-tariff/edit-tariff.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NotificationService } from 'src/app/notification.service';
@@ -9,7 +9,7 @@ import { ApiService } from '../services/api.service';
   templateUrl: './edit-tariff.component.html',
   styleUrls: ['./edit-tariff.component.sass']
 })
-export class EditTariffComponent {
+export class EditTariffComponent implements OnInit {
   tariffForm !: FormGroup;
   tariffName = '';
   ustList: string[] = ["Kg", "Lbs"];
