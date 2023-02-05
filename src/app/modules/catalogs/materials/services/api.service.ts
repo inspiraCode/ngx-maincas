251fc13597194eadeb9cda55407803e9e84cfb1c
Diego Torres Fuerte <dtorresf@redhat.com>
2023-02-05 09:24:07 -0700

    prepare material service api

--- src/app/modules/catalogs/materials/services/api.service.ts
+++ src/app/modules/catalogs/materials/services/api.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postMaterial(data: any) {
+    data.roles = data.roles.toString();
+    return this.http.post<any>(this.baseUrl + "/api/material/create", data);
+  }
+
+  updateMaterial(id: Number, data: any) {
+    data.roles = data.roles.toString();
+    return this.http.put<any>(this.baseUrl + "/api/material/" + id, data);
+  }
+
+  getMaterial() {
+    return this.http.get<any>(this.baseUrl + "/api/material/list");
+  }
+
+  getMaterialById(id: Number) {
+    return this.http.get<any>(this.baseUrl + "/api/material/" + id);
+  }
+
+  deleteMaterialById(id: Number) {
+    return this.http.delete<any>(this.baseUrl + "/api/material/" + id);
+  }
 }
