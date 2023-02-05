14ebb00606ffbee4ee2460f7d29f23c5b00dd6fe
Diego Torres Fuerte <dtorresf@redhat.com>
2023-02-04 20:43:46 -0700

    update api endpoint for tariff schedules

--- src/app/modules/catalogs/tariff-schedules/services/api.service.ts
+++ src/app/modules/catalogs/tariff-schedules/services/api.service.ts
@@ -1,30 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   postTariff(data: any) {
-    return this.http.post<any>("http://localhost:3000/tariffs/", data);
+    return this.http.post<any>(this.baseUrl + "/api/tariff-schedule/create", data);
   }
 
   updateTariff(id: Number, data: any) {
-    return this.http.put<any>("http://localhost:3000/tariffs/" + id, data);
+    return this.http.put<any>(this.baseUrl + "/api/tariff-schedule/" + id, data);
   }
 
   getTariff() {
-    return this.http.get<any>("http://localhost:3000/tariffs/");
+    return this.http.get<any>(this.baseUrl + "/api/tariff-schedule/list");
   }
 
   getTariffById(id: Number) {
-    return this.http.get<any>("http://localhost:3000/tariffs/" + id);
+    return this.http.get<any>(this.baseUrl + "/api/tariff-schedule/" + id);
   }
 
   deleteTariffById(id: Number) {
-    return this.http.delete<any>("http://localhost:3000/tariffs/" + id);
+    return this.http.delete<any>(this.baseUrl + "/api/tariff-schedule/" + id);
   }
 }
